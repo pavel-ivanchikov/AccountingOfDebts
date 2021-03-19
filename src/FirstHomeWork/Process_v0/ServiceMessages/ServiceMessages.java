826f2ae5d0b8e2eaf1567f18de5b9762d34095a5
pavel-ivanchikov <78387594+pavel-ivanchikov@users.noreply.github.com>
2021-03-20 01:15:39 +0300

    Упростил Журнал, ты увидишь, Паша, запусти тест. Написал служебные сообщения на все случаи жизни.

---
+++ src/FirstHomeWork/Process_v0/ServiceMessages/ServiceMessages.java
@@ -0,0 +1,8 @@
+package FirstHomeWork.Process_v0.ServiceMessages;
+
+public enum ServiceMessages {
+    IGV, ITK, SDL,   // debt's
+    NDB, SNM,        // person's
+    NPR,             // myLife's
+    OPN, CLS    // common
+}
