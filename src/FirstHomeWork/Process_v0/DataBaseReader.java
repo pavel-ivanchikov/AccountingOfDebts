7a1d0352f4659b0d19e3f2915f908b97201e10c1
pavel-ivanchikov <78387594+pavel-ivanchikov@users.noreply.github.com>
2021-03-28 12:37:27 +0300

    Создал заготовку для чтения из файлов, но больше тестирую работу в новой ветке. В этой ветке дальше буду делать инициализацию "сообщение за сообщением".

---
+++ src/FirstHomeWork/Process_v0/DataBaseReader.java
@@ -0,0 +1,14 @@
+package FirstHomeWork.Process_v0;
+
+public class DataBaseReader {
+
+    // для первого процесса
+    DataBaseReader() {
+
+    }
+
+    // для не первого процесса
+    DataBaseReader(long id) {
+
+    }
+}
